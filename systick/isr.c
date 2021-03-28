45cb9771faa76797cf7559112d6a8943027edb0f
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-03-28 20:11:09 +0800

    add new example of systick

---
+++ systick/isr.c
@@ -0,0 +1,17 @@
+void NMI_Handler(void)
+{
+	while(1);
+}
+
+void HardFault_Handler(void)
+{
+	while(1);
+}
+
+void SVC_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
---
+++ systick/main.c
@@ -0,0 +1,86 @@
+#include "stm32f0xx_ll_crs.h"
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_exti.h"
+#include "stm32f0xx_ll_cortex.h"
+#include "stm32f0xx_ll_utils.h"
+#include "stm32f0xx_ll_pwr.h"
+#include "stm32f0xx_ll_dma.h"
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_tim.h"
+
+void system_clock_init(void)
+{
+	/* power on */
+	LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+	/* setup flash */
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+	while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_1);
+
+	/* set system clock by 48Mhz with HSI source and PLL circuit */
+
+	/* enable HSI and wait untill ready */
+	while(LL_RCC_HSI_IsReady() != 1);
+	LL_RCC_HSI_SetCalibTrimming(16);
+	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2, LL_RCC_PLL_MUL_12);
+	LL_RCC_PLL_Enable();
+
+	/* enable PLL and wait untill ready */
+	LL_RCC_PLL_Enable();
+	while(LL_RCC_PLL_IsReady() != 1);
+	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+	/* enable system clock and wait utill ready */
+	while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+	LL_SetSystemCoreClock(48000000);
+}
+
+void delay(volatile uint32_t count)
+{
+	while(count--);
+}
+
+void gpio_init(void)
+{
+	/* rcc initialization */
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_8 | LL_GPIO_PIN_9);
+
+	/* gpio initialization */
+	LL_GPIO_InitTypeDef gpio_init_struct = {
+	 	.Pin = LL_GPIO_PIN_8 | LL_GPIO_PIN_9,
+	  	.Mode = LL_GPIO_MODE_OUTPUT,
+		.Speed = LL_GPIO_SPEED_FREQ_HIGH,
+		.OutputType = LL_GPIO_OUTPUT_PUSHPULL,
+		.Pull = LL_GPIO_PULL_NO
+	};
+
+	LL_GPIO_Init(GPIOC, &gpio_init_struct);
+}
+
+void systick_init(void)
+{
+	LL_InitTick(48000000, 10); //10Hz
+	LL_SYSTICK_EnableIT();
+}
+
+void SysTick_Handler(void)
+{
+	LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8 | LL_GPIO_PIN_9);
+}
+
+int main(void)
+{
+	system_clock_init();
+	gpio_init();
+	systick_init();
+
+	while(1) {
+	}
+}
