7318cf3abbebb59e79ded3deb8a1d3354f4d10d5
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-02-28 19:03:43 +0800

    timer_cascade: fix synchronization of timer 1 and 2

--- timer_cascade/main.c
+++ timer_cascade/main.c
@@ -45,25 +45,6 @@ void delay(volatile uint32_t count)
 	while(count--);
 }
 
-void led_init(void)
-{
-	/* rcc initialization */
-	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
-
-	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_8 | LL_GPIO_PIN_9);
-
-	/* gpio initialization */
-	LL_GPIO_InitTypeDef gpio_init_struct = {
-	 	.Pin = LL_GPIO_PIN_8 | LL_GPIO_PIN_9,
-	  	.Mode = LL_GPIO_MODE_OUTPUT,
-		.Speed = LL_GPIO_SPEED_FREQ_HIGH,
-		.OutputType = LL_GPIO_OUTPUT_PUSHPULL,
-		.Pull = LL_GPIO_PULL_NO
-	};
-
-	LL_GPIO_Init(GPIOC, &gpio_init_struct);
-}
-
 void gpio_init(void)
 {
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
@@ -113,9 +94,7 @@ void timer1_init(void)
 
 	LL_TIM_DisableARRPreload(TIM1);
 
-	/* set timer 1 as master to the timer 2 */
-	LL_TIM_SetSlaveMode(TIM1, LL_TIM_SLAVEMODE_TRIGGER);
-	LL_TIM_SetTriggerInput(TIM1, LL_TIM_TS_ITR2);
+	/* set timer 1 as master of the timer 2 */
 	LL_TIM_SetTriggerOutput(TIM1, LL_TIM_TRGO_OC1REF);
 	LL_TIM_EnableMasterSlaveMode(TIM1);
 
@@ -126,7 +105,7 @@ void timer1_init(void)
 		.OCMode = LL_TIM_OCMODE_PWM1,
 		.OCState = LL_TIM_OCSTATE_DISABLE,
 		.OCNState = LL_TIM_OCSTATE_DISABLE,
-		.CompareValue = 250, //duty = 1200/1600 = 75%
+		.CompareValue = 800,
 		.OCPolarity = LL_TIM_OCPOLARITY_HIGH
 	};
 	LL_TIM_OC_Init(TIM1, LL_TIM_CHANNEL_CH1, &timer_oc_init_struct);
@@ -161,7 +140,7 @@ void timer2_init(void)
 	/* 48MHz / (1 * 320) = 150KHz */
 	LL_TIM_InitTypeDef timer_init_struct = {
 		.Prescaler = 1 - 1,
-		.Autoreload = 1/*320 - 1*/,
+		.Autoreload = 320 - 1,
 		.CounterMode = LL_TIM_COUNTERMODE_UP,
 		.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1,
 	};
@@ -169,53 +148,37 @@ void timer2_init(void)
 
 	LL_TIM_DisableARRPreload(TIM2);
 
+#if 1   /* turn on and off of this could to observe the difference */
 	/* enable slave mode and set input trigger from timer1 */
 	LL_TIM_SetTriggerInput(TIM2, LL_TIM_TS_ITR0);
 	LL_TIM_SetSlaveMode(TIM2, LL_TIM_SLAVEMODE_RESET);
 	LL_TIM_DisableIT_TRIG(TIM2);
 	LL_TIM_DisableDMAReq_TRIG(TIM2);
+	LL_TIM_DisableMasterSlaveMode(TIM2);
 	LL_TIM_SetTriggerOutput(TIM2, LL_TIM_TRGO_UPDATE);
-	LL_TIM_EnableMasterSlaveMode(TIM2);
-	LL_TIM_SetClockSource(TIM2, LL_TIM_CLOCKSOURCE_EXT_MODE1);
-
-	/*=====================================*
-	 * timer output channel initialization *
-	 *=====================================*/
-	LL_TIM_OC_InitTypeDef timer_oc_init_struct = {
-		.OCMode = LL_TIM_OCMODE_PWM1,
-		.OCState = LL_TIM_OCSTATE_DISABLE,
-		.OCNState = LL_TIM_OCSTATE_DISABLE,
-		.OCPolarity = LL_TIM_OCPOLARITY_HIGH,
-		.OCNPolarity = LL_TIM_OCPOLARITY_HIGH,
-		.OCState = LL_TIM_OCSTATE_DISABLE,
-		.OCNState = LL_TIM_OCSTATE_DISABLE,
-	};
-	LL_TIM_OC_Init(TIM2, LL_TIM_CHANNEL_CH1, &timer_oc_init_struct);
-
-	LL_TIM_OC_DisableFast(TIM2, LL_TIM_CHANNEL_CH1);
+#endif
 
 	/*======================*
 	 * activate the timer 2 *
 	 *======================*/
-	LL_TIM_EnableIT_UPDATE(TIM2);
+	LL_TIM_EnableIT_CC1(TIM2);
 	LL_TIM_EnableCounter(TIM2);
 }
 
 void TIM2_IRQHandler(void)
 {
-	if(LL_TIM_IsActiveFlag_UPDATE(TIM2) == 1) {
-		LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8 | LL_GPIO_PIN_9);
-
+	if(LL_TIM_IsActiveFlag_CC1(TIM2) == 1) {
 		LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_13);
 
-		LL_TIM_ClearFlag_UPDATE(TIM2);
+		LL_TIM_ClearFlag_CC1(TIM2);
 	}
 }
 
 int main(void)
 {
+	/* probe pa8 and pb13 with the oscilloscope to observe the result */
+
 	system_clock_init();
-	led_init();
 	gpio_init();
 	timer1_init();
 	timer2_init();
